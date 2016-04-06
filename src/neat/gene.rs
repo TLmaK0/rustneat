d2ebeb01ddde7f3dee1a912a9f720feba8e4887f
Hugo Freire <hfreire@abajar.com>
2016-04-06 19:54:34 +0200

    Fix after rename neurons

--- src/neat/gene.rs
+++ src/neat/gene.rs
@@ -4,8 +4,8 @@ use std::cmp::Ordering;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Gene{
-    pub in_node_id: u32,
-    pub out_node_id: u32,
+    pub in_neuron_id: u32,
+    pub out_neuron_id: u32,
     pub weight: f64,
     pub enabled: bool,
 }
@@ -15,7 +15,7 @@ impl Eq for Gene {
 
 impl PartialEq for Gene{
     fn eq(&self, other: &Gene) -> bool {
-        self.in_node_id == other.in_node_id && self.out_node_id == other.out_node_id
+        self.in_neuron_id == other.in_neuron_id && self.out_neuron_id == other.out_neuron_id
     }
 }
 
@@ -23,13 +23,13 @@ impl Ord for Gene{
     fn cmp(&self, other: &Gene) -> Ordering {
         if self == other {
             Ordering::Equal
-        }else if self.in_node_id == other.in_node_id {
-            if self.out_node_id > other.out_node_id {
+        }else if self.in_neuron_id == other.in_neuron_id {
+            if self.out_neuron_id > other.out_neuron_id {
                 Ordering::Greater
             }else {
                 Ordering::Less
             }
-        }else if self.in_node_id > other.in_node_id {
+        }else if self.in_neuron_id > other.in_neuron_id {
             Ordering::Greater
         } else {
             Ordering::Less
@@ -51,6 +51,6 @@ impl Gene {
 
 impl Default for Gene{
     fn default () -> Gene {
-        Gene { in_node_id: 1, out_node_id: 1, weight: Gene::generate_weight(), enabled: true }
+        Gene { in_neuron_id: 1, out_neuron_id: 1, weight: Gene::generate_weight(), enabled: true }
     }
 }
