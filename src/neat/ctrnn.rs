8364a2c52240cc11719538af2f6b0edba2d3f4fe
Hugo Freire <hfreire@abajar.com>
2016-09-18 21:22:38 +0200

    Added collenchyma

--- src/neat/ctrnn.rs
+++ src/neat/ctrnn.rs
@@ -1,7 +1,33 @@
-pub struct Ctrnn;
+extern crate collenchyma;
+extern crate collenchyma_nn;
+
+use self::collenchyma::framework::IFramework;
+use self::collenchyma::backend::{Backend, BackendConfig};
+use self::collenchyma::frameworks::Native;
+use self::collenchyma::hardware::IHardware;
+use self::collenchyma::device::IDevice;
+use self::collenchyma::binary::IBinary;
+use self::collenchyma::memory::IMemory;
+use self::collenchyma::Error;
+use self::collenchyma::tensor::SharedTensor;
+use self::collenchyma_nn::*;
+
+pub struct Ctrnn {
+    pub backend: Backend<Native>
+}
 
 impl Ctrnn {
-    pub fn activate(steps: usize, gamma: &Vec<f64>, delta_t: f64, tau: &Vec<f64>, wij: &Vec<Vec<f64>>, theta: &Vec<f64>, wik: &Vec<Vec<f64>>, i: &Vec<f64>) -> Vec<f64> {
+    pub fn new() -> Ctrnn{
+        let framework = Native::new();
+        let hardwares = &framework.hardwares().to_vec();
+        let backend_config = BackendConfig::new(framework, hardwares);
+        let backend = Backend::new(backend_config);
+        Ctrnn {
+            backend: backend.unwrap()
+        }
+    }
+
+    pub fn activate(&self, steps: usize, gamma: &Vec<f64>, delta_t: f64, tau: &Vec<f64>, wij: &Vec<Vec<f64>>, theta: &Vec<f64>, wik: &Vec<Vec<f64>>, i: &Vec<f64>) -> Vec<f64> {
         return vec![0.0, 0.0];
     }
 }
@@ -24,14 +50,16 @@ mod tests {
                             vec![-3.45899, -1.27388]];
         let i =             vec![0.98856, 0.31540];
 
-        assert_eq!( Ctrnn::activate(1, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+        let ctrnn = Ctrnn::new();
+
+        assert_eq!( ctrnn.activate(1, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-0.86998, -6.11057, -2.14179]);
-        assert_eq!( Ctrnn::activate(10, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+        assert_eq!( ctrnn.activate(10, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7698, -4.9550, -3.1953]);
-        assert_eq!( Ctrnn::activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+        assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7869, -4.9408, -3.2092]);
 
-        assert_eq!( Ctrnn::activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            Ctrnn::activate(100, &gamma, delta_t, &tau, &wij, &theta, &wik, &i));
+        assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+            ctrnn.activate(100, &gamma, delta_t, &tau, &wij, &theta, &wik, &i));
     }
 }
