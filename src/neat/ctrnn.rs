af273a40dda7ed9748502b96e342ca5b5caa4e69
Hugo Freire <hfreire@abajar.com>
2016-09-20 21:10:02 +0200

    First sigmoid with tensors and collenchyma

--- src/neat/ctrnn.rs
+++ src/neat/ctrnn.rs
@@ -4,16 +4,16 @@ extern crate collenchyma_nn;
 use self::collenchyma::framework::IFramework;
 use self::collenchyma::backend::{Backend, BackendConfig};
 use self::collenchyma::frameworks::Native;
-use self::collenchyma::hardware::IHardware;
-use self::collenchyma::device::IDevice;
-use self::collenchyma::binary::IBinary;
-use self::collenchyma::memory::IMemory;
 use self::collenchyma::Error;
 use self::collenchyma::tensor::SharedTensor;
+use self::collenchyma::DeviceType;
+use self::collenchyma::MemoryType;
 use self::collenchyma_nn::*;
+use self::collenchyma::tensor::IntoTensorDesc;
 
 pub struct Ctrnn {
-    pub backend: Backend<Native>
+    pub backend: Backend<Native>, //shuld replace with general backend
+    pub cpu: DeviceType
 }
 
 impl Ctrnn {
@@ -22,13 +22,53 @@ impl Ctrnn {
         let hardwares = &framework.hardwares().to_vec();
         let backend_config = BackendConfig::new(framework, hardwares);
         let backend = Backend::new(backend_config);
+        let native = Native::new();
         Ctrnn {
-            backend: backend.unwrap()
+            backend: backend.unwrap(),
+            cpu: native.new_device(native.hardwares()).unwrap() 
         }
     }
 
-    pub fn activate(&self, steps: usize, gamma: &Vec<f64>, delta_t: f64, tau: &Vec<f64>, wij: &Vec<Vec<f64>>, theta: &Vec<f64>, wik: &Vec<Vec<f64>>, i: &Vec<f64>) -> Vec<f64> {
+    pub fn activate(&self, steps: usize, gamma_v: &Vec<f64>, delta_t: f64, tau_v: &Vec<f64>, wij_v: &Vec<Vec<f64>>, theta_v: &Vec<f64>, wik_v: &Vec<Vec<f64>>, i_v: &Vec<f64>) -> Vec<f64> {
+        let mut gamma = self.vector_to_tensor(gamma_v);
+        let mut tau = self.vector_to_tensor(tau_v);
+        let mut wij = self.matrix_to_tensor(wij_v);
+        let mut theta = self.vector_to_tensor(theta_v);
+        let mut wik = self.matrix_to_tensor(wik_v);
+        let mut i = self.vector_to_tensor(i_v);
+        let mut result = SharedTensor::<f64>::new(self.backend.device(), wij.desc()).unwrap();
+        &self.backend.sigmoid(&mut wij, &mut result).unwrap();
+        //result.add_device(&cpu).unwrap();
+        result.sync(&self.cpu).unwrap();
         return vec![0.0, 0.0];
+    }
+
+    fn matrix_to_tensor(&self, matrix: &Vec<Vec<f64>>) -> SharedTensor<f64> {
+        let mut tensor = SharedTensor::<f64>::new(self.backend.device(), &(matrix.len(), matrix[0].len())).unwrap();
+        //tensor.add_device(&self.cpu).unwrap();
+        tensor.sync(&self.cpu).unwrap();
+        self.write_to_memory(tensor.get_mut(&self.cpu).unwrap(), &matrix.iter().flat_map(|s| s.iter()).collect::<Vec<&f64>>());
+        tensor
+    }
+
+    fn vector_to_tensor(&self, vector: &Vec<f64>) -> SharedTensor<f64> {
+        self.generic_to_tensor(&vec!(vector.len()), vector)
+    }
+
+    fn generic_to_tensor(&self, desc: &Vec<usize>, data: &[f64]) -> SharedTensor<f64> {
+        let mut tensor = SharedTensor::<f64>::new(self.backend.device(), desc).unwrap();
+        //tensor.add_device(&self.cpu).unwrap();
+        tensor.sync(&self.cpu).unwrap();
+        self.write_to_memory(tensor.get_mut(&self.cpu).unwrap(), data);
+        tensor
+    }
+
+    fn write_to_memory<T: Copy>(&self, mem: &mut MemoryType, data: &[T]) {
+        let &mut MemoryType::Native(ref mut mem) = mem;
+        let mut mem_buffer = mem.as_mut_slice::<T>();
+        for (index, datum) in data.iter().enumerate() {
+            mem_buffer[index] = *datum;
+        }
     }
 }
 
