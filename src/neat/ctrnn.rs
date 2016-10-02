663e6c3da0992dbf52b9c18cf018aedaba6bf95c
Hugo Freire <hfreire@abajar.com>
2016-10-02 20:30:59 +0200

    Able to calculate first step of ctrnn

--- src/neat/ctrnn.rs
+++ src/neat/ctrnn.rs
@@ -1,74 +1,40 @@
-extern crate collenchyma;
-extern crate collenchyma_nn;
+extern crate rulinalg;
 
-use self::collenchyma::framework::IFramework;
-use self::collenchyma::backend::{Backend, BackendConfig};
-use self::collenchyma::frameworks::Native;
-use self::collenchyma::Error;
-use self::collenchyma::tensor::SharedTensor;
-use self::collenchyma::DeviceType;
-use self::collenchyma::MemoryType;
-use self::collenchyma_nn::*;
-use self::collenchyma::tensor::IntoTensorDesc;
+use self::rulinalg::vector::Vector;
+use self::rulinalg::matrix::Matrix;
+use self::rulinalg::matrix::BaseMatrix;
+use self::rulinalg::matrix::BaseMatrixMut;
 
 pub struct Ctrnn {
-    pub backend: Backend<Native>, //shuld replace with general backend
-    pub cpu: DeviceType
 }
 
 impl Ctrnn {
     pub fn new() -> Ctrnn{
-        let framework = Native::new();
-        let hardwares = &framework.hardwares().to_vec();
-        let backend_config = BackendConfig::new(framework, hardwares);
-        let backend = Backend::new(backend_config);
-        let native = Native::new();
-        Ctrnn {
-            backend: backend.unwrap(),
-            cpu: native.new_device(native.hardwares()).unwrap() 
-        }
+        Ctrnn {}
     }
 
     pub fn activate(&self, steps: usize, gamma_v: &Vec<f64>, delta_t: f64, tau_v: &Vec<f64>, wij_v: &Vec<Vec<f64>>, theta_v: &Vec<f64>, wik_v: &Vec<Vec<f64>>, i_v: &Vec<f64>) -> Vec<f64> {
-        let mut gamma = self.vector_to_tensor(gamma_v);
-        let mut tau = self.vector_to_tensor(tau_v);
-        let mut wij = self.matrix_to_tensor(wij_v);
-        let mut theta = self.vector_to_tensor(theta_v);
-        let mut wik = self.matrix_to_tensor(wik_v);
-        let mut i = self.vector_to_tensor(i_v);
-        let mut result = SharedTensor::<f64>::new(self.backend.device(), wij.desc()).unwrap();
-        &self.backend.sigmoid(&mut wij, &mut result).unwrap();
-        //result.add_device(&cpu).unwrap();
-        result.sync(&self.cpu).unwrap();
+        let mut gamma = Ctrnn::matrix_from_vector(gamma_v); 
+        let mut theta = Ctrnn::matrix_from_vector(theta_v); 
+        let mut wij = Ctrnn::matrix_from_vectors(wij_v); 
+        let mut wik = Ctrnn::matrix_from_vectors(wik_v); 
+        let mut i = Ctrnn::matrix_from_vector(i_v); 
+        let mut tau = Ctrnn::matrix_from_vector(tau_v);
+        let mut result = &gamma + (tau.apply( &(|x| 1.0/x) ) * delta_t).elemul(&( (&wij * ( &gamma - &theta ).apply(&Ctrnn::sigmoid)) - &gamma + (&wik * &i)));
+        println!("{:?}", result); 
         return vec![0.0, 0.0];
     }
 
-    fn matrix_to_tensor(&self, matrix: &Vec<Vec<f64>>) -> SharedTensor<f64> {
-        let mut tensor = SharedTensor::<f64>::new(self.backend.device(), &(matrix.len(), matrix[0].len())).unwrap();
-        //tensor.add_device(&self.cpu).unwrap();
-        tensor.sync(&self.cpu).unwrap();
-        self.write_to_memory(tensor.get_mut(&self.cpu).unwrap(), &matrix.iter().flat_map(|s| s.iter()).collect::<Vec<&f64>>());
-        tensor
+    fn sigmoid(y: f64) -> f64 {
+        1f64 / (1f64 + (-y).exp())
     }
 
-    fn vector_to_tensor(&self, vector: &Vec<f64>) -> SharedTensor<f64> {
-        self.generic_to_tensor(&vec!(vector.len()), vector)
+    fn matrix_from_vector(vector: &Vec<f64>) -> Matrix<f64> {
+        Matrix::new(vector.len(), 1, vector.as_slice())
     }
 
-    fn generic_to_tensor(&self, desc: &Vec<usize>, data: &[f64]) -> SharedTensor<f64> {
-        let mut tensor = SharedTensor::<f64>::new(self.backend.device(), desc).unwrap();
-        //tensor.add_device(&self.cpu).unwrap();
-        tensor.sync(&self.cpu).unwrap();
-        self.write_to_memory(tensor.get_mut(&self.cpu).unwrap(), data);
-        tensor
-    }
-
-    fn write_to_memory<T: Copy>(&self, mem: &mut MemoryType, data: &[T]) {
-        let &mut MemoryType::Native(ref mut mem) = mem;
-        let mut mem_buffer = mem.as_mut_slice::<T>();
-        for (index, datum) in data.iter().enumerate() {
-            mem_buffer[index] = *datum;
-        }
+    fn matrix_from_vectors(vectors: &Vec<Vec<f64>>) -> Matrix<f64> {
+        Matrix::new(vectors.len(), vectors[1].len(), vectors.iter().flat_map(|s| s.iter().cloned()).collect::<Vec<f64>>())
     }
 }
 
@@ -93,12 +59,16 @@ mod tests {
         let ctrnn = Ctrnn::new();
 
         assert_eq!( ctrnn.activate(1, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+            vec![-0.33246, -4.03171, -1.43741]);
+
+        assert_eq!( ctrnn.activate(2, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-0.86998, -6.11057, -2.14179]);
         assert_eq!( ctrnn.activate(10, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7698, -4.9550, -3.1953]);
         assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7869, -4.9408, -3.2092]);
 
+        //converges
         assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             ctrnn.activate(100, &gamma, delta_t, &tau, &wij, &theta, &wik, &i));
     }
