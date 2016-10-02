f007885405981a680977ae6ccc71088d9b636006
Hugo Freire <hfreire@abajar.com>
2016-10-02 21:18:24 +0200

    Ctrnn passes tests

--- src/neat/ctrnn.rs
+++ src/neat/ctrnn.rs
@@ -1,6 +1,5 @@
 extern crate rulinalg;
 
-use self::rulinalg::vector::Vector;
 use self::rulinalg::matrix::Matrix;
 use self::rulinalg::matrix::BaseMatrix;
 use self::rulinalg::matrix::BaseMatrixMut;
@@ -15,14 +14,16 @@ impl Ctrnn {
 
     pub fn activate(&self, steps: usize, gamma_v: &Vec<f64>, delta_t: f64, tau_v: &Vec<f64>, wij_v: &Vec<Vec<f64>>, theta_v: &Vec<f64>, wik_v: &Vec<Vec<f64>>, i_v: &Vec<f64>) -> Vec<f64> {
         let mut gamma = Ctrnn::matrix_from_vector(gamma_v); 
-        let mut theta = Ctrnn::matrix_from_vector(theta_v); 
-        let mut wij = Ctrnn::matrix_from_vectors(wij_v); 
-        let mut wik = Ctrnn::matrix_from_vectors(wik_v); 
-        let mut i = Ctrnn::matrix_from_vector(i_v); 
-        let mut tau = Ctrnn::matrix_from_vector(tau_v);
-        let mut result = &gamma + (tau.apply( &(|x| 1.0/x) ) * delta_t).elemul(&( (&wij * ( &gamma - &theta ).apply(&Ctrnn::sigmoid)) - &gamma + (&wik * &i)));
-        println!("{:?}", result); 
-        return vec![0.0, 0.0];
+        let theta = Ctrnn::matrix_from_vector(theta_v); 
+        let wij = Ctrnn::matrix_from_vectors(wij_v); 
+        let wik = Ctrnn::matrix_from_vectors(wik_v); 
+        let i = Ctrnn::matrix_from_vector(i_v); 
+        let tau = Ctrnn::matrix_from_vector(tau_v);
+        let delta_t_tau = tau.apply( &(|x| 1.0/x) ) * delta_t;
+        for _ in 0..steps { 
+            gamma = &gamma + delta_t_tau.elemul(&( (&wij * ( &gamma - &theta ).apply(&Ctrnn::sigmoid)) - &gamma + (&wik * &i)));
+        }
+        return gamma.into_vec();
     }
 
     fn sigmoid(y: f64) -> f64 {
@@ -59,17 +60,17 @@ mod tests {
         let ctrnn = Ctrnn::new();
 
         assert_eq!( ctrnn.activate(1, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            vec![-0.33246, -4.03171, -1.43741]);
+            vec![-0.3324610969203632, -4.031708339680356, -1.4374137183957871]);
 
         assert_eq!( ctrnn.activate(2, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            vec![-0.86998, -6.11057, -2.14179]);
+            vec![-0.8747989167571583, -6.213498344386237, -2.143379723316894]);
         assert_eq!( ctrnn.activate(10, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            vec![-1.7698, -4.9550, -3.1953]);
+            vec![-1.7607615233242453, -4.96208021858224, -3.1877336197565627]);
         assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            vec![-1.7869, -4.9408, -3.2092]);
+            vec![-1.7868613827907782, -4.940744845752775, -3.2091966016693183]);
 
         //converges
-        assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
-            ctrnn.activate(100, &gamma, delta_t, &tau, &wij, &theta, &wik, &i));
+        assert_eq!( ctrnn.activate(100, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
+            vec![-1.7868662273977671, -4.940740909854486, -3.2092005096958243]);
     }
 }
