71d0475f7709190631aa50f76573370fcf83b027
Hugo Freire <hfreire@abajar.com>
2016-10-03 21:31:58 +0200

    Activate neurons with ctrnn

--- src/neat/ctrnn.rs
+++ src/neat/ctrnn.rs
@@ -12,11 +12,11 @@ impl Ctrnn {
         Ctrnn {}
     }
 
-    pub fn activate(&self, steps: usize, gamma_v: &Vec<f64>, delta_t: f64, tau_v: &Vec<f64>, wij_v: &Vec<Vec<f64>>, theta_v: &Vec<f64>, wik_v: &Vec<Vec<f64>>, i_v: &Vec<f64>) -> Vec<f64> {
+    pub fn activate(&self, steps: usize, gamma_v: &Vec<f64>, delta_t: f64, tau_v: &Vec<f64>, wij_v: &(usize, usize, Vec<f64>), theta_v: &Vec<f64>, wik_v: &(usize, usize, Vec<f64>), i_v: &Vec<f64>) -> Vec<f64> {
         let mut gamma = Ctrnn::matrix_from_vector(gamma_v); 
         let theta = Ctrnn::matrix_from_vector(theta_v); 
-        let wij = Ctrnn::matrix_from_vectors(wij_v); 
-        let wik = Ctrnn::matrix_from_vectors(wik_v); 
+        let wij = Ctrnn::matrix_from_matrix(wij_v); 
+        let wik = Ctrnn::matrix_from_matrix(wik_v); 
         let i = Ctrnn::matrix_from_vector(i_v); 
         let tau = Ctrnn::matrix_from_vector(tau_v);
         let delta_t_tau = tau.apply( &(|x| 1.0/x) ) * delta_t;
@@ -34,8 +34,8 @@ impl Ctrnn {
         Matrix::new(vector.len(), 1, vector.as_slice())
     }
 
-    fn matrix_from_vectors(vectors: &Vec<Vec<f64>>) -> Matrix<f64> {
-        Matrix::new(vectors.len(), vectors[1].len(), vectors.iter().flat_map(|s| s.iter().cloned()).collect::<Vec<f64>>())
+    fn matrix_from_matrix(matrix: &(usize, usize, Vec<f64>)) -> Matrix<f64> {
+        Matrix::new(matrix.0, matrix.1, matrix.2.as_slice())
     }
 }
 
@@ -48,13 +48,13 @@ mod tests {
         let gamma =         vec![0.0, 0.0, 0.0];
         let delta_t =       13.436;
         let tau =           vec![61.694, 10.149, 16.851];
-        let wij =           vec![vec![-2.94737, 2.70665, -0.57046],
-                            vec![-3.27553,   3.67193, 1.83218],
-                            vec![2.32476,   0.24739, 0.58587]];
+        let wij =           (3, 3, vec![-2.94737, 2.70665, -0.57046,
+                            -3.27553,   3.67193, 1.83218,
+                            2.32476,   0.24739, 0.58587]);
         let theta =         vec![-0.695126, -0.677891, -0.072129];
-        let wik =           vec![vec![-0.10097, -3.04457],
-                            vec![-4.86594, 1.79273],
-                            vec![-3.45899, -1.27388]];
+        let wik =           (3, 2, vec![-0.10097, -3.04457,
+                            -4.86594, 1.79273,
+                            -3.45899, -1.27388]);
         let i =             vec![0.98856, 0.31540];
 
         let ctrnn = Ctrnn::new();
@@ -64,8 +64,10 @@ mod tests {
 
         assert_eq!( ctrnn.activate(2, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-0.8747989167571583, -6.213498344386237, -2.143379723316894]);
+
         assert_eq!( ctrnn.activate(10, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7607615233242453, -4.96208021858224, -3.1877336197565627]);
+
         assert_eq!( ctrnn.activate(30, &gamma, delta_t, &tau, &wij, &theta, &wik, &i),
             vec![-1.7868613827907782, -4.940744845752775, -3.2091966016693183]);
 
