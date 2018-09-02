d3c191de6c37b782d1e4a69bc3db4a0f8a86849d
Hugo Freire <hfreire@abajar.com>
2018-09-02 15:16:20 +0200

    Added bias

--- graphs/approximation.js
+++ graphs/approximation.js
@@ -1,20 +1,26 @@
 var svg, paths = []; 
 
-var rangeX, rangeY;
+var lineChart;
 
 function approximation_init(id){
   svg = d3.select('#' + id).append('svg');
   var width = $('#' + id).children().width() / 2;
-  rangeX = [-width, width];
+  var rangeX = [-width, width];
   var height = $('#' + id).children().height();
-  rangeY = [0, height];
+  var rangeY = [0, height];
 
   for(var n=0; n<10; n++ ) {
     paths[n] = svg.append('path');
     paths[n].attr("transform", "translate(" + width + ")");
     paths[n].attr("stroke", "rgb(" + (50 + n * 20) + ",0,0)");
   }
-  paths[0].attr("stroke", "rgb(0,0,0)");
+  paths[0].attr("stroke", "rgb(200,200,200)");
+  scaleX = d3.scaleLinear().domain([-10, 10]).range(rangeX);
+  scaleY = d3.scaleLinear().domain([0, 100]).range(rangeY);
+
+  lineChart = d3.line()
+                    .x(function(d){ return scaleX(d[0]); })
+                    .y(function(d){ return height - scaleY(d[1]); });
 }
 
 var functionToApproximate = [];
@@ -22,17 +28,10 @@ var functionApproximations = [];
 
 function approximation(id, value){
   if (functionToApproximate.length == 0) functionToApproximate = value;
-
   if (functionApproximations.length == 9) functionApproximations.shift();
 
   functionApproximations.push(value);
 
-  var scaleX = d3.scaleLinear().domain([-10, 10]).range(rangeX);
-  var scaleY = d3.scaleLinear().domain([0, 100]).range(rangeY);
-
-  var lineChart = d3.line()
-                    .x(function(d){ return scaleX(d[0]); })
-                    .y(function(d){ return d[1]; });
   paths[0].attr("d", lineChart(functionToApproximate));
 
   for(var n=0; n < functionApproximations.length; n++ ){
