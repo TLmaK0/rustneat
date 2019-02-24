9cf25768d6fe62e04e7ade85e79a06f0d63fcc30
Erlend Langseth <3rlendhl@gmail.com>
2019-02-24 15:41:22 +0100

    Generic rewrite
    - Genome is a trait that implements mutation, mating, distance measure etc.
            (opens up for more experimentation and extensibility)
    - The default Genome implementor is NeuralNetwork which implements said functionality
            pretty much just as before. So NeuralNetwork is the new Genome.
            Some slight changes in how bias works had to be done to be able to mirror the way
            they do it in some paper that introduced or explained CTRNN iirc.
    - Thus now we pass around Organisms not Genomes
    
    - And..:
        - Update rand
        - Removed lots of unnecessary f64
        - Remove CtrnnNeuralNetwork (can just use Ctrnn for setting up the
            network)
        - Fix an error in species

--- tests/rustneat_tests.rs
+++ tests/rustneat_tests.rs
@@ -1,21 +1,20 @@
-extern crate rustneat;
 
 #[cfg(test)]
 mod test {
-    use rustneat::{Environment, Organism, Population};
+    use rustneat::{Environment, Organism, Population, NeuralNetwork};
 
     struct MyEnvironment;
 
-    impl Environment for MyEnvironment {
-        fn test(&self, _: &mut Organism) -> f64 {
+    impl Environment<NeuralNetwork> for MyEnvironment {
+        fn test(&self, _: &mut NeuralNetwork) -> f64 {
             0.1234f64
         }
     }
 
     struct XORClassification;
 
-    impl Environment for XORClassification {
-        fn test(&self, organism: &mut Organism) -> f64 {
+    impl Environment<NeuralNetwork> for XORClassification {
+        fn test(&self, organism: &mut NeuralNetwork) -> f64 {
             let mut output = vec![0f64];
             let mut distance: f64;
             organism.activate(vec![0f64, 0f64], &mut output);
@@ -26,19 +25,19 @@ mod test {
             distance += (1f64 - output[0]).abs();
             organism.activate(vec![1f64, 1f64], &mut output);
             distance += (0f64 - output[0]).abs();
-            (4f64 - distance).powi(2)
+            16.0 / (1.0 + distance)
         }
     }
 
     #[test]
     fn should_be_able_to_generate_a_population() {
-        let population = Population::create_population(150);
+        let population = Population::<NeuralNetwork>::create_population(150);
         assert!(population.size() == 150);
     }
 
     #[test]
     fn population_can_evolve() {
-        let mut population = Population::create_population(1);
+        let mut population = Population::<NeuralNetwork>::create_population(1);
         population.evolve();
         let genome = &population.get_organisms()[0].genome;
         assert_eq!(genome.total_genes(), 1);
@@ -47,7 +46,7 @@ mod test {
 
     #[test]
     fn population_can_be_tested_on_environment() {
-        let mut population = Population::create_population(10);
+        let mut population = Population::<NeuralNetwork>::create_population(10);
         let mut environment = MyEnvironment;
         population.evaluate_in(&mut environment);
         assert!(population.get_organisms()[0].fitness == 0.1234f64);
@@ -55,9 +54,9 @@ mod test {
 
     #[test]
     fn network_should_be_able_to_solve_xor_classification() {
-        let mut population = Population::create_population(150);
+        let mut population = Population::<NeuralNetwork>::create_population(150);
         let mut environment = XORClassification;
-        let mut champion_option: Option<Organism> = None;
+        let mut champion_option: Option<Organism<NeuralNetwork>> = None;
         while champion_option.is_none() {
             population.evolve();
             population.evaluate_in(&mut environment);
@@ -67,9 +66,10 @@ mod test {
                 }
             }
         }
-        let champion = champion_option.as_mut().unwrap();
+        let Organism {genome: champion, fitness: _} = champion_option.as_mut().unwrap();
         let mut output = vec![0f64];
         champion.activate(vec![0f64, 0f64], &mut output);
+        // println!("Output[0] = {}", output[0]);
         assert!(output[0] < 0.1f64);
         champion.activate(vec![0f64, 1f64], &mut output);
         assert!(output[0] > 0.9f64);
