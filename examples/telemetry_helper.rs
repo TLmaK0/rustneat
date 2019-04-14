90d107fae9eda37f95da58ab8268d3ab259064e3
Hugo Freire <hfreire@abajar.com>
2019-04-14 12:31:11 +0200

    Add telemetry for start to check ctrnn implementation

--- examples/telemetry_helper.rs
+++ examples/telemetry_helper.rs
@@ -16,7 +16,11 @@ pub fn enable_telemetry(query_string: &str, open: bool) {
     dashboard.add_graph("fitness1", "fitness", 0, 0, 4, 4);
     dashboard.add_graph("network1", "network", 4, 0, 4, 4);
     dashboard.add_graph("approximation1", "approximation", 0, 4, 2, 2);
+    run_server(dashboard, query_string, open);
+}
 
+#[cfg(feature = "telemetry")]
+pub fn run_server(dashboard: Dashboard, query_string: &str, open: bool){
     rusty_dashed::Server::serve_dashboard(dashboard);
 
     let url = format!("http://localhost:3000{}", query_string);
