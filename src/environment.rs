6d9a20498cf3f89b2c3d56d89ab19e1b2a6f7c22
Hugo Freire <hugofreiregil@gmail.com>
2020-10-25 14:10:55 +0100

    Allow single thread environments

--- src/environment.rs
+++ src/environment.rs
@@ -5,4 +5,10 @@ pub trait Environment: Sync {
     /// This test will return the value required by this enviroment to test
     /// against
     fn test(&self, organism: &mut Organism) -> f64;
+
+    /// Returns the number of threads to use on evaluation.
+    /// Implement this method to use single thread environment returning 1.
+    fn threads(&self) -> usize {
+        return num_cpus::get()
+    }
 }
